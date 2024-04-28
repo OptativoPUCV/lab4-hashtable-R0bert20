d18420c49903f14e17b02d33e7968dcff561fdba
R0bert20 <163351866+R0bert20@users.noreply.github.com>
2024-04-28 00:49:36 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -122,6 +122,16 @@ Pair * firstMap(HashMap * map) {
 
 
 Pair * nextMap(HashMap * map) {
+    if(map == NULL || map->buckets == NULL) return NULL;
+    map->current++;
+    map->current%=map->capacity;
+    while(map->current != -1){
+      if(map->buckets[map->current]!=NULL && map->buckets[map->current]->key!=NULL){
+        return map->buckets[map->current];
+      }
+      map->current++;
+      map->current%=map->capacity;
+    }
 
     return NULL;
 }
