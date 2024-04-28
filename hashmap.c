fb2099c20face1e2b88cd8d18a2a5c3ffe3c4fee
R0bert20 <163351866+R0bert20@users.noreply.github.com>
2024-04-28 00:46:28 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -80,16 +80,17 @@ HashMap * createMap(long capacity) {
 }
 
 void eraseMap(HashMap * map,  char * key) {    
-    if (map == NULL || map->buckets == NULL) return;
+    if(map == NULL || map->buckets == NULL) return;
+    if(key == NULL) return;
     long posicion = hash(key,map->capacity);
     while(map->buckets[posicion]!=NULL && map->buckets[posicion]->key!=NULL){
         if(is_equal(map->buckets[posicion]->key,key)){
-          map->buckets[posicion]=NULL;
-          map->size--;
-          return;
+            map->buckets[posicion]->key=NULL;
+            map->size--;
+            return;
         }
-      posicion++;
-      posicion%=map->capacity;
+        posicion++;
+        posicion%=map->capacity;
     }
 }
 
